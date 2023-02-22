9ebae62dca29040d3fbd58635049c41e2080617d
anhnt-jpn <anh.nguyen@go-jpn.com>
2023-02-22 16:21:02 +0700

    JP_MNT_DEV-260 fix stripe template

---
+++ app/design/frontend/Coke/jp_marche/Magento_Checkout/web/js/view/payment/default-mixin.js
@@ -0,0 +1,46 @@
+define([
+        'ko',
+        'jquery',
+        'uiComponent',
+        'Magento_Checkout/js/action/place-order',
+        'Magento_Checkout/js/action/select-payment-method',
+        'Magento_Checkout/js/model/quote',
+        'Magento_Customer/js/model/customer',
+        'Magento_Checkout/js/model/payment-service',
+        'Magento_Checkout/js/checkout-data',
+        'Magento_Checkout/js/model/checkout-data-resolver',
+        'uiRegistry',
+        'Magento_Checkout/js/model/payment/additional-validators',
+        'Magento_Ui/js/model/messages',
+        'uiLayout',
+        'Magento_Checkout/js/action/redirect-on-success'
+    ], function (
+    ko,
+    $,
+    Component,
+    placeOrderAction,
+    selectPaymentMethodAction,
+    quote,
+    customer,
+    paymentService,
+    checkoutData,
+    checkoutDataResolver,
+    registry,
+    additionalValidators,
+    Messages,
+    layout,
+    redirectOnSuccessAction
+    ) {
+        'use strict';
+
+        var mixin = {
+            isChecked: ko.computed(function () {
+                return quote.paymentMethod() ? quote.paymentMethod().method : 'stripe_payments';
+            }),
+        };
+
+        return function (target) {
+            return target.extend(mixin);
+        };
+    }
+);
--- app/design/frontend/Coke/jp_marche/requirejs-config.js
+++ app/design/frontend/Coke/jp_marche/requirejs-config.js
@@ -6,6 +6,9 @@ var config = {
             },
             'Aheadworks_Sarp2/js/product/subscription-details': {
                 'Aheadworks_Sarp2/js/product/subscription-details-mixin': true
+            },
+            'Magento_Checkout/js/view/payment/default': {
+                'Magento_Checkout/js/view/payment/default-mixin': true
             }
         }
     },
